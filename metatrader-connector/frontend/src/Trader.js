88a2eb5c7928db2d63224b5eecf609d71e83c64e
slawyn <unixtracer@gmail.com>
2023-09-05 23:05:51 +0200

    REACT: Rewrites row highlighting

--- metatrader-connector/frontend/src/Trader.js
+++ metatrader-connector/frontend/src/Trader.js
@@ -8,53 +8,45 @@ const Button = ({ name, customClass }) => {
     );
 };
 
-class Trader extends React.Component {
-    constructor(props) {
-        super(props);
-        this.tableRef = props.table;
-        this.data = props.data;
-        this.rowRef = React.createRef();
-    }
+function Trader(props) {
 
-    componentDidMount() {
-    }
+    return (
 
-    render() {
-        return (
-            <table>
-                <tbody>
-                    <tr>
-                        <td>
-                            <form>
-                                <button type="submit">Save to Google</button>
-                            </form>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <Button name={"Buy[Market]"} customClass={"bluebutton"} />
-                        </td>
-                        <td>
-                            <Button name={"Buy[Limit]"} customClass={"bluebutton"} />
-                        </td>
-                        <td>
-                            <Button name={"Buy[Stop]"} customClass={"bluebutton"} />
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <Button name={"Sell[Market]"} customClass={"redbutton"} />
-                        </td>
-                        <td>
-                            <Button name={"Sell[Limit]"} customClass={"redbutton"} />
-                        </td>
-                        <td>
-                            <Button name={"Sell[Stop]"} customClass={"redbutton"} />
-                        </td>
-                    </tr>
-                </tbody>
-            </table>)
-    }
+        <table>
+            <tbody>
+                <tr>
+                    <td>
+                        <form>
+                            <button type="submit">Save to Google</button>
+                        </form>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <Button name={"Buy[Market]"} customClass={"bluebutton"} />
+                    </td>
+                    <td>
+                        <Button name={"Buy[Limit]"} customClass={"bluebutton"} />
+                    </td>
+                    <td>
+                        <Button name={"Buy[Stop]"} customClass={"bluebutton"} />
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <Button name={"Sell[Market]"} customClass={"redbutton"} />
+                    </td>
+                    <td>
+                        <Button name={"Sell[Limit]"} customClass={"redbutton"} />
+                    </td>
+                    <td>
+                        <Button name={"Sell[Stop]"} customClass={"redbutton"} />
+                    </td>
+                </tr>
+            </tbody>
+        </table >
+    )
+
 }
 
 
