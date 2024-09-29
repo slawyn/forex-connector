3a7fb9e8221189b9d1e53b98b2cd4ff4b2742d77
slawyn <unixtracer@gmail.com>
2024-09-29 22:28:19 +0200

    adds 'talib' library for technical analysis

--- backend/backtester/strategy/smacross.py
+++ backend/backtester/strategy/smacross.py
@@ -1,11 +1,14 @@
 from backtesting import Strategy
 from backtesting.lib import crossover
-from backtesting.test import SMA
-
+import talib
+import pandas as pd
 
 class SmaCross(Strategy):
-    n1 = 10
+    n1 = 5
     n2 = 20
+    n = 20
+    dev = 2
+
     calculator = None
     ratio = 2.25
     risk_amount = 5
@@ -13,8 +16,20 @@ class SmaCross(Strategy):
 
     def init(self):
         close = self.data.Close
-        self.sma1 = self.I(SMA, close, self.n1)
-        self.sma2 = self.I(SMA, close, self.n2)
+        
+        # Calculate SMAs using TA-Lib
+        self.sma1 = self.I(lambda: talib.SMA(close, timeperiod=self.n1), name='SMA1')
+        self.sma2 = self.I(lambda: talib.SMA(close, timeperiod=self.n2), name='SMA2')
+
+        # Calculate Bollinger Bands using TA-Lib
+        self.upper_band, self.middle_band, self.lower_band = talib.BBANDS(
+            close, timeperiod=self.n, nbdevup=self.dev, nbdevdn=self.dev, matype=0
+        )
+
+        # Register for plotting
+        self.I(lambda: self.upper_band, name='Upper Band')
+        self.I(lambda: self.middle_band, name='Middle Band')
+        self.I(lambda: self.lower_band, name='Lower Band')
 
     def _get_current_trades(self):
         current_equity = self.equity
@@ -25,8 +40,6 @@ class SmaCross(Strategy):
         if self.position:
             print(f"Current position size: {self.position.size}, Entry price: {self.position.pl}")
 
-
-
     def next(self):
         price = self.data.Close[-1]
         spread = self.data.Spread[-1]
@@ -34,9 +47,8 @@ class SmaCross(Strategy):
         parameters = self.calculator.calculate_stoploss(price, spread, self.risk_amount, self.risk_lot)
 
         if crossover(self.sma1, self.sma2):
-            print("BUY: ",parameters.ask, parameters.sl_buy, parameters.tp_buy)
-            self.buy(size=self.risk_lot, sl=parameters.sl_buy, tp = parameters.tp_buy)
+            print("BUY: ", parameters.ask, parameters.sl_buy, parameters.tp_buy)
+            self.buy(size=self.risk_lot, sl=parameters.sl_buy, tp=parameters.tp_buy)
         elif crossover(self.sma2, self.sma1):
-            print("SELL: ",parameters.bid, parameters.sl_sell, parameters.tp_sell)
-            self.sell(size=self.risk_lot, sl=parameters.sl_sell, tp = parameters.tp_sell)
-
+            print("SELL: ", parameters.bid, parameters.sl_sell, parameters.tp_sell)
+            self.sell(size=self.risk_lot, sl=parameters.sl_sell, tp=parameters.tp_sell)
